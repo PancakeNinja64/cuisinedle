f9e991348706a4026b0257cd73b83a49fb796672
Zakari <zbautista@csus.edu>
2026-04-23 13:39:35 -0700

    Render Dishdle game board

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import { GameBoard } from "@/components/GameBoard";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-zinc-50 dark:bg-zinc-950">
+      <GameBoard />
+    </main>
+  );
+}
